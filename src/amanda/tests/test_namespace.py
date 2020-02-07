4a668072c47e29584ce023edee392d183bcebe9c
uchuhimo <uchuhimo@outlook.com>
2020-02-07 09:30:10 +0800

    test: fix tests for namespace

--- src/amanda/tests/test_namespace.py
+++ src/amanda/tests/test_namespace.py
@@ -6,11 +6,11 @@ from amanda.namespace import get_base_name, get_namespace, is_qualified
 
 @pytest.fixture
 def namespace():
-    return Namespace("/level1/level2")
+    return Namespace("level1/level2")
 
 
 def test_qualified(namespace):
-    assert namespace.qualified("level3") == "level1/level2/level3"
+    assert namespace.qualified("level3") == "/level1/level2/level3"
 
 
 def test_belong_to(namespace):
@@ -26,7 +26,7 @@ def test_eq(namespace):
 
 
 def test_repr(namespace):
-    assert repr(namespace) == "Namespace(/level1/level2)"
+    assert repr(namespace) == "Namespace(level1/level2)"
 
 
 def test_is_qualified():
