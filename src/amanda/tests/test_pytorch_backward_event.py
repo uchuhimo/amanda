9b38266b0b94ad8e99bcd721e59cbd2985d5e14f
ChandlerGuan <bonboru.guan@gmail.com>
2021-07-13 19:51:34 +0800

    update: backward graph after hook

--- src/amanda/tests/test_pytorch_backward_event.py
+++ src/amanda/tests/test_pytorch_backward_event.py
@@ -4,7 +4,7 @@ import torchvision
 
 
 
-def test_naive_backward_func_hook():
+def test_naive_backward_func_hook(model=None):
     class CallbackCnt:
         def __init__(self):
             self.registration_cnt = 0
@@ -14,16 +14,15 @@ def test_naive_backward_func_hook():
         def registration_callback(self):
             self.registration_cnt += 1
 
-        def trigger_callback(self):
+        def trigger_callback(self, handle):
             self.trigger_cnt += 1
+            handle.remove()
 
         def add_hook(self, grad_fn):
             def _add_hook(grad_fn):
                 if grad_fn and grad_fn not in self.hooked_func:
-                    if grad_fn.__class__.__name__ == "AccumulateGrad":
-                        return
-                    grad_fn.register_hook(
-                        lambda in_tensor, out_tensor: self.trigger_callback()
+                    handle = grad_fn.register_hook(
+                        lambda in_tensor, out_tensor: self.trigger_callback(handle)
                     )
                     self.registration_callback()
                     self.hooked_func.append(grad_fn)
@@ -36,7 +35,8 @@ def test_naive_backward_func_hook():
 
     cnt = CallbackCnt()
 
-    model = torchvision.models.resnet50()
+    if not model:
+        model = torchvision.models.resnet50()
     x = torch.rand((2, 3, 227, 227))
     y = model(x)
 
@@ -44,11 +44,13 @@ def test_naive_backward_func_hook():
 
     y.backward(torch.rand_like(y))
 
-    print(f"registrition: {cnt.registration_cnt} trigger: {cnt.trigger_cnt}")
+    print(f"registration: {cnt.registration_cnt} trigger: {cnt.trigger_cnt}")
     assert cnt.registration_cnt == cnt.trigger_cnt
 
+    return cnt.trigger_cnt
 
-def test_amanda_backward_func_hook():
+
+def test_amanda_backward_func_hook(model=None):
     class TestTool(amanda.Tool):
         def __init__(self):
             super(TestTool, self).__init__(namespace="amanda/testtool")
@@ -76,7 +78,8 @@ def test_amanda_backward_func_hook():
         def after_backward_callback(self, context):
             self.after_cnt += 1
 
-    model = torchvision.models.resnet50()
+    if not model:
+        model = torchvision.models.resnet50()
     x = torch.rand((2, 3, 227, 227))
 
     tool = TestTool()
@@ -91,3 +94,17 @@ def test_amanda_backward_func_hook():
     assert tool.before_fw_cnt == tool.after_fw_cnt
     assert tool.before_cnt <= tool.after_fw_cnt
     assert tool.before_cnt <= tool.after_cnt
+
+    return tool.after_cnt
+
+
+def test_amanda_backward_graph_trace():
+
+    model = torchvision.models.resnet50()
+
+    amanda_cnt = test_amanda_backward_func_hook(model)
+    baseline_cnt = test_naive_backward_func_hook(model)
+
+    print(amanda_cnt)
+
+    assert baseline_cnt == amanda_cnt
